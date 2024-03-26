ecc5c5e33aedaef746590b26f1d26be6a4eaa567
Junheepark0531 <rusu0531@uw.edu>
2024-03-26 16:05:54 -0700

    modified flask app and searchbar

--- app.py
+++ app.py
@@ -9,13 +9,22 @@ def index():
 @app.route('/search', methods=['POST'])
 def search():
     # Perform search logic here if needed
-    
+    if request.method == 'POST':
+        search_query = request.form['search_query']
+        print(f"Search Query: {search_query}")
     # Redirect to result_page.html
     return redirect(url_for('result_page'))
 
 @app.route('/result_page')
 def result_page():
-    return render_template('result_page.html')
+    data = {
+        'percentage': 80,
+        'stability': 'unstable',
+        'cell': 'human 293T cells',
+        'group': 'Group 1',
+        'strands': 'High C Strands'
+    }
+    return render_template('result_page.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
