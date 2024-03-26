11d81c7f8fb523d22f267cbc9b2215a5036bcc6d
rohith42 <36945963+rohith42@users.noreply.github.com>
2024-03-25 19:39:41 -0700

    Finished PredictionPipeline

---
+++ predict.py
@@ -0,0 +1,48 @@
+from transformers import (
+  AutoTokenizer, 
+  AutoModelForSequenceClassification
+)
+import torch
+from torch.nn import functional as F
+
+MAX_SEQ_LEN = 150
+MODEL_PATH = "model-5e-05-96-0.001-10/checkpoint-40"  # Local path
+TOKENIZER = "qilowoq/AbLang_heavy"  # From HuggingFace
+
+
+class PredictionPipeline:
+  def __init__(self, sequence: str):
+    if len(sequence) > MAX_SEQ_LEN:
+      raise ValueError(
+        f"Sequence is too long.\nSequence length = {len(sequence)}, MAX_SEQ_LEN = {MAX_SEQ_LEN}"
+      )
+    self.seq = sequence
+  
+  def predict(self):
+    # Load model and tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(TOKENIZER)
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
+    
+    # Process and tokenize input sequence
+    processed_seq = ' '.join(self.seq)
+    encoded_input = tokenizer(processed_seq, return_tensors='pt')
+
+    # Run sequence through model to get stability prediction
+    with torch.no_grad():
+      logits = model(**encoded_input).logits
+      assert logits.size() == torch.Size([1, 2])
+
+    prediction = logits.argmax().item()
+    assert prediction in [0, 1]
+    likelihood = F.softmax(logits, dim=-1).squeeze()[prediction].item()
+
+    outputs = {0: "UNSTABLE", 1: "STABLE"}
+
+    return {
+      'prediction' : outputs[prediction],
+      'likelihood' : likelihood
+    }
+
+
+
+     
